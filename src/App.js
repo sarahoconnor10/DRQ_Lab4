ce38a62c21ba3a2a6c206e686be7a31bdf240a33
Sarah O'Connor <sarahaoconnor10@gmail.com>
2024-10-04 10:44:55 +0000

    Updated App.js and added Content.js

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function App() {
       Add hello world title and use Date() to display current time  
     */
     <div className="App">
-
-      <h1>Hello World:)</h1>
-      <h2>It is currently {new Date().toLocaleTimeString()}</h2>
+      <Content></Content>
     </div>
   );
 }
---
+++ src/Content.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Content = () => {
+    return (
+        /*
+          Add hello world title and use Date() to display current time  
+        */
+        <div className="App">
+    
+          <h1>Hello World:)</h1>
+          <h2>It is currently {new Date().toLocaleTimeString()}</h2>
+        </div>
+      );
+}
+export default Content;

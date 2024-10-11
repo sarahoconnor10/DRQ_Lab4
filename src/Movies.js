feed1f6951149bd0495c801d329dc5ba5858cc9d
Sarah O'Connor <sarahaoconnor10@gmail.com>
2024-10-11 10:51:49 +0000

    created Movies.js and added movie data to Read.js

---
+++ src/Movies.js
@@ -0,0 +1,14 @@
+// New movies component to display movie data
+
+
+const Movies = (props) => {
+    return(
+        <div>
+            movies component
+            {console.log(props.myMovies)}
+            
+        </div>
+    )
+};
+
+export default Movies;
--- src/Read.js
+++ src/Read.js
@@ -1,7 +1,36 @@
 //New component to hold Read content
-
+import Movies
+    from "./Movies";
 const Read = () => {
-    return <h3>Hello from Read.js!</h3>;
+    const movies = [
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+        },
+        {
+            "Title": "World War Z",
+            "Year": "2013",
+            "imdbID": "tt0816711",
+            "Type": "movie",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+        }
+    ];
+    return (
+        <div>
+            <Movies myMovies = {movies}/>
+            
+        </div>
+    ) 
 };
 
 export default Read;

fb581a0a24d8625ce19daa1783f2e41306e218e7
Sarah O'Connor <sarahaoconnor10@gmail.com>
2024-10-11 11:30:26 +0000

    Tidied and commented MovieItem.js

--- src/MovieItem.js
+++ src/MovieItem.js
@@ -1,15 +1,32 @@
-// Functional component to be responsible
-// for rendering individual movie details
+// 
+/*
+    Functional component to be responsible for rendering 
+    individual movie details.
+
+    - Import Card component from Bootstrap for UI
+    - Import useEffect from React to manage side effects in 
+      function components
+ */
 import Card from 'react-bootstrap/Card';
 import { useEffect } from "react";
 
 
 const MovieItem = (props) => {
+    /*
+        useEffect -> 
+            - React hook
+            - Ideal for synchronising component with external API's etc.
+            - Helps to handle side effects within functional components
+    */
     useEffect(() => {
         console.log("Movie Item:", props.mymovie);
     }, [props.mymovie]); 
 
     return (
+        /*
+            Use Bootstrap Cards to structure the output nicely.
+             * blockquote = extended quotation within JSX
+        */
         <div>
             <Card>
                 <Card.Body>

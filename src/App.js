62eef50986e6bd4c6c9862776f49f8e983d1db30
Sarah O'Connor <sarahaoconnor10@gmail.com>
2024-10-11 11:17:29 +0000

    Tidied and commented App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-
+//import all necessary components
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Content from './Content.js'
-import Header from './Header.js'
-import Footer from './Footer.js'
 import NavigationBar from './NavigationBar.js';
 import Create from './Create.js';
 import Read from './Read.js';
-
 import './App.css';
 
 function App() {
   return (
-
     /*
       Import NavigationBar and Route to different components
+        Home -> routes to <Content />
+        Create -> routes to <Create />
+        Read -> routes to <Read/> 
+                  links to <Movies/> 
+                    links to <MovieItem/> which displays bootstrap card 
+                    with each movie title, poster, and release year.        
     */
     <div className="App">
       <Router>
         <NavigationBar />
-
         <Routes>
           <Route path="/" element={<Content />} />
-          <Route path="/read" element={<Read />} />
           <Route path="/create" element={<Create />} />
+          <Route path="/read" element={<Read />} />
         </Routes>
       </Router>
     </div>
--- src/MovieItem.js
+++ src/MovieItem.js
@@ -7,15 +7,15 @@ import { useEffect } from "react";
 const MovieItem = (props) => {
     useEffect(() => {
         console.log("Movie Item:", props.mymovie);
-  }, [props.mymovie]); // Only run this effect when the mymovie prop changes
-    
-    return(
+    }, [props.mymovie]); 
+
+    return (
         <div>
             <Card>
-                <Card.Header>
-                    {props.myMovie.Title}
-                </Card.Header>
                 <Card.Body>
+                    <Card.Title>
+                        {props.myMovie.Title}
+                    </Card.Title>
                     <blockquote className="blockquote mb-0">
                         <img src={props.myMovie.Poster}></img>
                         <footer>{props.myMovie.Year}</footer>

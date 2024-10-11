831cbf517ef0f01b74a96e28b0d058a80a1d24ff
Sarah O'Connor <sarahaoconnor10@gmail.com>
2024-10-11 11:03:27 +0000

    added displaying movie year & poster under title

--- src/MovieItem.js
+++ src/MovieItem.js
@@ -5,6 +5,8 @@ const MovieItem = (props) => {
     return(
         <div>
             <h3>{props.myMovie.Title}</h3>
+            <h4>{props.myMovie.Year}</h4>
+            <img src={props.myMovie.Poster}></img>
         </div>
     );
 }

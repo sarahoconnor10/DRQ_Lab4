9cffc98779ecb995b17736ac8494b04569deaf4b
Sarah O'Connor <sarahaoconnor10@gmail.com>
2024-10-18 11:06:49 +0000

    Create.js form created and logging input to console

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,49 @@
 //New component to hold Create content
+import { useState } from "react";
 
 const Create = () => {
-    return <h3>Hello from Create.js!</h3>;
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(title);
+        console.log(year);
+        console.log(poster);
+    }
+    const [title, setTitle] = useState('');
+    const [year, setYear] = useState('');
+    const [poster, setPoster] = useState('');
+
+    return (
+        <div>
+            <h3>Hello from Create</h3>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Add Movie Title</label>
+                    <input  type="text" 
+                            className = "form-control" 
+                            value = {title}
+                            onChange = {(e) => { setTitle(e.target.value) }}/>
+                </div>
+                <div>
+                    <label>Add Movie Year</label>
+                    <input  type="text" 
+                            className = "form-control" 
+                            value = {year}
+                            onChange = {(e) => { setYear(e.target.value) }}/>
+                </div>
+                <div>
+                    <label>Add Movie Poster</label>
+                    <input  type="text" 
+                            className = "form-control" 
+                            value = {poster}
+                            onChange = {(e) => { setPoster(e.target.value) }}/>
+                </div>
+                <div>
+                    <input type="submit" value = "Add Movie"></input>
+                </div>
+
+            </form>
+        </div>
+    );
 };
 
 export default Create;
--- src/Read.js
+++ src/Read.js
@@ -1,34 +1,56 @@
 //New component to hold Read content
 import Movies
     from "./Movies";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Read = () => {
     /* 
         - Store json movie data in a const variable.
         - Return this const into Movies component for external use.
     */
-    const movies = [
-        {
-            "Title": "Avengers: Infinity War",
-            "Year": "2018",
-            "imdbID": "tt4154756",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Captain America: Civil War",
-            "Year": "2016",
-            "imdbID": "tt3498820",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-        },
-        {
-            "Title": "World War Z",
-            "Year": "2013",
-            "imdbID": "tt0816711",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-        }
-    ];
+    // const movies = [
+    //     {
+    //         "Title": "Avengers: Infinity War",
+    //         "Year": "2018",
+    //         "imdbID": "tt4154756",
+    //         "Type": "movie",
+    //         "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+    //     },
+    //     {
+    //         "Title": "Captain America: Civil War",
+    //         "Year": "2016",
+    //         "imdbID": "tt3498820",
+    //         "Type": "movie",
+    //         "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+    //     },
+    //     {
+    //         "Title": "World War Z",
+    //         "Year": "2013",
+    //         "imdbID": "tt0816711",
+    //         "Type": "movie",
+    //         "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+    //     }
+    // ];
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(
+        () => {
+            axios.get('https://jsonblob.com/api/jsonblob/1287718524221775872')
+                .then((response) => {
+                    console.log(response.data);
+                    setMovies(response.data.movies);
+                })
+                .catch(
+                    (error) => {
+                        console.log(error)
+
+                    }
+                )
+        }, []
+    );
+
     return (
         <div>
             <Movies myMovies={movies} />
